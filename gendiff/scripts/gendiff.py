b26ab694d14a20001e95a2ab40079a8de28cec2c
Abdujabbar Mirkhalikov <abdujabbar.mirkhalikov@gmail.com>
2022-02-28 21:38:04 +0500

    init the project

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,16 @@
+import argparse
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Generate diff.')
+    parser.add_argument('first_file', metavar='first_file', type=str, nargs=1)
+    parser.add_argument('second_file', metavar='second_file', type=str, nargs=1)
+
+    args = parser.parse_args()
+
+    print(args)
+
+
+if __name__ == '__main__':
+    main()
