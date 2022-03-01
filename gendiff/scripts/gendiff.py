81520bd2c0e0d6cf5db58a0d28b2339295a60433
Abdujabbar Mirkhalikov <abdujabbar.mirkhalikov@gmail.com>
2022-03-01 22:22:51 +0500

    Added format optional param

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,6 +6,8 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff.')
     parser.add_argument('first_file', metavar='first_file', type=str, nargs=1)
     parser.add_argument('second_file', metavar='second_file', type=str, nargs=1)
+    
+    parser.add_argument('-f', '--format', help='set format of output')
 
     args = parser.parse_args()
 
