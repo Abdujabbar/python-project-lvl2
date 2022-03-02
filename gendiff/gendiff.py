b77c9e902e5e6ccf852cdc4b7d199c6055bd8490
Abdujabbar Mirkhalikov <abdujabbar.mirkhalikov@gmail.com>
2022-03-03 00:22:03 +0500

    Finished step3

---
+++ gendiff/gendiff.py
@@ -0,0 +1,47 @@
+from ftplib import all_errors
+import yaml
+import json
+
+def get_file_contents(path, format):
+    with open(path, 'r') as stream:
+        if format == 'json':
+
+            return json.load(stream)
+        
+        return yaml.safe_load(stream)
+        
+
+
+def generate_diff(file1, file2, format='json'):
+    content1 = get_file_contents(file1, format)
+    content2 = get_file_contents(file2, format)
+    all_keys = sorted(set(list(content1.keys()) + list(content2.keys())))
+    result = "{\n"
+    pads = "    "
+    
+    for key in all_keys:
+        if key in content1 and key in content2:
+            if content2[key] != content1[key]:
+                result += f"{pads} - {key}: {str(content1.get(key)).lower()}\n"
+                result += f"{pads} + {key}: {str(content2.get(key)).lower()}"
+            else:
+                result += f"{pads}   {key}: {str(content1.get(key)).lower()}"
+        elif key not in content1:
+            result += f"{pads} - {key}: {str(content2.get(key)).lower()}"
+        elif key not in content2:
+            result += f"{pads} + {key}: {str(content1.get(key)).lower()}"
+        else:
+            result += f"{pads}   {key}: {str(content1.get(key)).lower()}"
+        
+        result += "\n"
+    
+    result += "}\n"
+
+    return result
+
+
+
+
+
+    # print(type(content1))
+    # print(type(content2))
---
+++ gendiff/main.py
@@ -0,0 +1,21 @@
+import argparse
+
+from .gendiff import generate_diff
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Generate diff.')
+    parser.add_argument('first_file', metavar='first_file')
+    parser.add_argument('second_file', metavar='second_file')
+    
+    parser.add_argument('-f', '--format', help='set format of output', default='json')
+
+    args = parser.parse_args()
+    
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
+
+
+
+if __name__ == '__main__':
+    main()
