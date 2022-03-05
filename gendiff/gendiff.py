c6895fc1321d533f358ec77420495d6c7d1d29e8
Abdujabbar Mirkhalikov <abdujabbar.mirkhalikov@gmail.com>
2022-03-06 00:07:24 +0500

    fix linting

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -16,18 +16,19 @@ def get_file_contents(path):
 def detect_operator_and_item(d1, d2, key):
     if key not in d1:
         return [(ADDED, d2.get(key))]
-    
+
     if key not in d2:
         return [(DELETED, d1.get(key))]
 
     if isinstance(d1[key], dict) and isinstance(d2[key], dict):
         return [(CHILD_CHANGED, dicts_diff(d1[key], d2[key]))]
-    
+
     if d1.get(key) != d2.get(key):
         return [(DELETED, d1.get(key)),
                 (ADDED, d2.get(key))]
-    
+
     return [(SAME, d1.get(key))]
+
 
 def dicts_diff(dict1, dict2):
     all_keys = sorted(set(list(dict1.keys()) + list(dict2.keys())))
